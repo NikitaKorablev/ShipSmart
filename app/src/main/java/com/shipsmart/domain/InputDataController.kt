15d4d7b271344efbc68bb83fdc592ed575bd4814
Никита Кораблев <kit06092003@gmail.com>
2024-08-02 14:26:37 +0300

    updated password validation in InputDataController

--- app/src/main/java/com/shipsmart/domain/InputDataController.kt
+++ app/src/main/java/com/shipsmart/domain/InputDataController.kt
@@ -30,23 +30,65 @@ class InputDataController(activity: AppCompatActivity) {
         val email = emailInput.text.toString()
         val password = passwordInput.text.toString()
 
+        if (!dataIsValid(email, password)) return
+        checkUserPassword(email, password)
+    }
+
+    private fun dataIsValid(email: String, password: String) : Boolean {
+        if (!emailIsValid(email)) {
+            toastConstructor.show("Email is incorrect")
+            return false
+        }
+
+        if (passwordIsValid(password)) {
+            toastConstructor.show("Password is incorrect")
+            return false
+        }
+
+        return true
+    }
+
+    private fun emailIsValid(email: String): Boolean {
+        if (email.isBlank() || email.isEmpty())
+            return false
+        if (!email.contains("@") || !email.contains("."))
+            return false
+        return true
+    }
+
+    private fun passwordIsValid(password: String): Boolean {
+        //TODO: add password validation
+        return password.length > 4
+    }
+
+    private fun checkUserPassword(email: String, password: String) {
         lifecycleScope.launch {
-            val user = withContext(Dispatchers.IO) {
-                return@withContext authorisationDB.getUser(email = email)
-            }
-            Log.e(TEST, "user is $user")
-            if (user == null) toastConstructor.show("This user undefined!")
-            else if (user.password != password) {
-                toastConstructor.show("Your password is incorrect!")
-            } else {
-                toastConstructor.show("Welcome back!")
+            try {
+                val user = withContext(Dispatchers.IO) {
+                    return@withContext authorisationDB.getUser(email = email)
+                }
+
+                if (user == null) {
+                    toastConstructor.show("User with this email is undefined!")
+                    return@launch
+                }
+                if (user.password != password) {
+                    toastConstructor.show("Your password is incorrect!")
+                    return@launch
+                }
+
                 // TODO: Start MainActivity
+                toastConstructor.show("Welcome back!")
+            } catch (e: Exception) {
+                Log.e(ERROR, e.message.toString())
+                toastConstructor.show("An error occurred!")
             }
         }
     }
 
     companion object {
         const val TAG = "InputDataController"
+        const val ERROR = "InputDataController.error"
         const val TEST = "InputDataControllerTest"
     }
 }
