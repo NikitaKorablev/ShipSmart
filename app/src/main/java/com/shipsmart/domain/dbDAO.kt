8c618007b9cfb1abbb7a3984b289ddf49a50d4b8
Никита Кораблев <kit06092003@gmail.com>
2024-08-09 22:41:33 +0300

    deleted unused files

--- app/src/main/java/com/shipsmart/domain/dbDAO.kt
+++
@@ -1,8 +0,0 @@
-package com.shipsmart.domain
-
-import com.shipsmart.data.dbLogic.SupabaseUser
-
-interface dbDAO {
-    suspend fun getUser(email: String) : SupabaseUser?
-    suspend fun addUser(user: SupabaseUser)
-}
